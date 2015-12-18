83e7a4c1382c1a189759f7935d8f4a1972270c32
Pedro Gorrin <pedro.gorrin@ngncloud.com>
2015-12-18 09:16:13 -0430

    initial commit

---
+++ src/Providers/LaravelElasticSearchServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Neokike\LaravelElasticsearch\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Config;
+
+class LaravelElasticSearchServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application.
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application.
+     */
+    public function register()
+    {
+
+    }
+
+
+}
